05c6745f8bd09571eb17a5cfdd30901b9487fe57
Witoslaw Koczewski <wi@koczewski.de>
2020-04-04 00:38:43 +0200

    durable-ratom

--- src/kcu/butils.cljs
+++ src/kcu/butils.cljs
@@ -1,15 +1,12 @@
 (ns kcu.butils
   "Browser Utilities"
   (:require
+   [reagent.core :as r]
    [kcu.utils :as u]))
 
 
 (defn as-local-storage-key [k]
-  (let [k (if (string? k)
-            k
-            (u/encode-edn k))
-        k (u/encode-edn k)]
-    k))
+  (if (string? k) k (u/encode-edn k)))
 
 
 (defn get-from-local-storage [k]
@@ -24,3 +21,13 @@
       .-localStorage
       (.setItem (as-local-storage-key k)
                 (u/encode-edn v))))
+
+
+(defn durable-ratom [k default-value]
+  (let [value (get-from-local-storage k)
+        ratom (r/atom (or value default-value))]
+    (add-watch ratom ::durable-ratom
+               (fn [_ _ old-val new-val]
+                 (when (not= old-val new-val)
+                   (set-to-local-storage k new-val))))
+    ratom))
